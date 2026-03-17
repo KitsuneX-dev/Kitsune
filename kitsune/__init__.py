28053f1e6a74924e6bade6204c43bb955fa7e157
KitsuneX-dev <yushi.kitsune.dev@gmail.com>
2026-03-17 23:30:34 +0400

    fix: init self.lvl, add proxy support

--- kitsune/__init__.py
+++ kitsune/__init__.py
@@ -1,11 +1,5 @@
-"""
-Kitsune Userbot
-Developer: Yushi (@Mikasu32)
-License: AGPLv3
-"""
+"""Kitsune database package."""
 
-__author__ = "Yushi"
-__contact__ = "@Mikasu32"
-__copyright__ = "Copyright 2024-2025, Yushi"
-__license__ = "AGPLv3"
-__status__ = "Production"
+from .manager import DatabaseManager, JSONValue
+
+__all__ = ["DatabaseManager", "JSONValue"]
