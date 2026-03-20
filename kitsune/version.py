4966cb3b4e3b0335dbba6182b68b41f459a9ed68
Yushi <yushi@kitsune.dev>
2026-03-20 22:17:41 +0400

    feat: version 1.2.0, Beta (Stable) status, uptime reset on restart

---
+++ kitsune/version.py
@@ -0,0 +1,21 @@
+"""Kitsune version info"""
+
+# © Yushi (@Mikasu32), 2024-2026
+# Kitsune Userbot
+# License: AGPLv3
+
+from __future__ import annotations
+
+import contextlib
+import os
+
+__version__ = (1, 2, 0)
+__version_str__ = ".".join(map(str, __version__))
+
+branch: str = "main"
+
+with contextlib.suppress(Exception):
+    import git
+
+    _repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    branch = git.Repo(path=_repo_path).active_branch.name
