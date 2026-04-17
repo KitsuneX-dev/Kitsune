9d372ff1bcf8bd4bcfea9483ad7e1a0d34bae9ca
KitsuneX-dev <yushi.kitsune.dev@gmail.com>
2026-04-18 00:19:35 +0400

    fix:backup

--- kitsune/kitsune/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__author__ = "Yushi"
-__contact__ = "@Mikasu32"
-__copyright__ = "Copyright 2024-2026, Yushi"
-__license__ = "AGPLv3"
-__status__ = "Production"
--- kitsune/kitsune/database/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .manager import DatabaseManager, JSONValue
-
-__all__ = ["DatabaseManager", "JSONValue"]
